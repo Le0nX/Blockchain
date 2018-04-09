7e319677fcaa173bd44f2fdbcb583460e6197b59
le0nx <cloudmindone@gmail.com>
2018-04-09 16:24:51 +0300

    init: created Base Block class

---
+++ X.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Denis Nefedov
+// Version     : 0.01
+// Copyright   : LGPL
+// Description : main source file.
+//============================================================================
+
+#include "Block.h"
+
+int main(void) {
+	Block first(0,0,"none","none");
+	first.info();
+	return 0;
+}
